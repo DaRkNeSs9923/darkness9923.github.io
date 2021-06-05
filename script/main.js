814bf619c0f87288c7f049dcd5337ca6d75551a7
Vratska <elias99elias23@gmail.com>
2021-06-05 17:40:23 -0300

    99%

--- script/main.js
+++ script/main.js
@@ -19,18 +19,30 @@ function validate(evt) {
     }
   }
 //--------------
-function calc1()
-{
+
+function calc1() {
   num1 = parseFloat(document.getElementById("unidade1").value) || 0;
   num2 = parseFloat(document.getElementById("unidade2").value) || 0;
   media = ((num1 + num2) / 2)
-  resultado = (12.5 - ((3/4)*(num1 + num2)));
-  if (media >= 7) {
-    document.getElementById("P1").innerHTML = "<pp id='pp'>Cara, se tu tem uma média 7, pra que tu vai fazer final?? wtf??</pp>";
-  } else {
-    document.getElementById("result1").innerHTML = (resultado).toFixed(2);
+  resultado = (12.5 - ((3 / 4) * (num1 + num2)));
+
+
+  if (resultado > 10) {
+    document.getElementById("P1").innerHTML = "<pp id='pp'>Cara, tu precisa tirar mais de 10 na final, acho que você ja entendeu...</pp>";
   }
-  
+  else if (media >= 7) {
+    document.getElementById("P1").innerHTML = "<pp id='pp'>Cara, se tu tem uma média maior ou igual a 7, pra que tu vai fazer final?? wtf??</pp>";
+  } else {
+    if ((document.getElementById("pp")) === null) {
+      document.getElementById("result1").innerHTML = (resultado).toFixed(2);
+
+    } else {
+      document.getElementById('pp').innerHTML = "<p1 id='P1'> você precisa tirar <span id='result1'></span> para ser aprovado.</p1>";
+      document.getElementById("result1").innerHTML = (resultado).toFixed(2);
+    }
+
+  }
+
 }
 
 function calc2() 
@@ -39,6 +51,26 @@ function calc2()
   num2 = parseFloat(document.getElementById("unidade22").value) || 0;
   final1 = parseFloat(document.getElementById("final11").value) || 0;
   resultado = ((num1 + num2) * 3 + final1 * 4) / 10;
-  document.getElementById("result2").innerHTML = (resultado).toFixed(2);
-  
+
+  if (resultado < 5) {
+    if ((document.getElementById("F2")) === null) {
+      document.getElementById("result2").innerHTML = "<p1 id='F1'>Infelizmente, não foi dessa vez, você ficou com média final de <span id='result22'></span></p1> ";
+      document.getElementById("result22").innerHTML = (resultado).toFixed(2);
+    } else {
+      document.getElementById("F2").innerHTML = "<span1 id='result2'></span1>";
+      document.getElementById("result2").innerHTML = "<p1 id='F1'>Infelizmente, não foi dessa vez, você ficou com média final de <span id='result22'></span></p1> ";
+      document.getElementById("result22").innerHTML = (resultado).toFixed(2);
+    }
+
+  } else if (resultado >= 5) {
+    if ((document.getElementById("F1")) === null) {
+      document.getElementById("result2").innerHTML = "<p1 id='F2'> Parabéns, sua média final ficou <span id='result22'></span></p2>";
+      document.getElementById("result22").innerHTML = (resultado).toFixed(2);
+    } else {
+      document.getElementById("F1").innerHTML = "<span1 id='result2'></span1>";
+      document.getElementById("result2").innerHTML = "<p1 id='F2'> Parabéns, sua média final ficou <span id='result22'></span></p2>";
+      document.getElementById("result22").innerHTML = (resultado).toFixed(2);
+    }
+  }
+
 }
